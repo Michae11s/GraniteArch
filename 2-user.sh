f8ed1a4a17b94f6184ebc2130c387b58251611cb
Michaels@Rivendell <Michae11s>
2021-11-25 12:56:20 -0500

    setup for my configs

--- 2-user.sh
+++ 2-user.sh
@@ -7,60 +7,79 @@
 #  ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
 #  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
 #-------------------------------------------------------------------------
-
+cd ~/GraniteArch
+source ./settings
+source ./Pkgs
+cd ~
+mkdir ~/.local
+mkdir ~/.local/bin
+ln -s ~/.local/bin/ ~/bin
+xdg-user-dirs-update
 echo -e "\nINSTALLING AUR SOFTWARE\n"
 # You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
 
-echo "CLONING: YAY"
-cd ~
-git clone "https://aur.archlinux.org/yay.git"
-cd ${HOME}/yay
+echo "CLONING: AURACLE"
+mkdir ~/build
+cd ~/build
+git clone "https://aur.archlinux.org/auracle-git.git"
+cd auracle-git
 makepkg -si --noconfirm
-cd ~
-touch "$HOME/.cache/zshhistory"
-git clone "https://github.com/ChrisTitusTech/zsh"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
-ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
-PKGS=(
-'autojump'
-'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
-'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
-'lightly-git'
-'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
-'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
-'noto-fonts-emoji'
-'papirus-icon-theme'
-'plasma-pa'
-'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
-'snapper-gui-git'
-'ttf-droid'
-'ttf-hack'
-'ttf-meslo' # Nerdfont package
-'ttf-roboto'
-'zoom' # video conferences
-'snap-pac'
-)
+echo "Loading Dotfiles"
+cd ~/build
+git config --global credential.helper store
+git clone https://github.com/Michae11s/dots.git
+cd dots
+./deployDots.sh
+mv ./.fehbg ~/
+~/.fehbg
 
-for PKG in "${PKGS[@]}"; do
-    yay -S --noconfirm $PKG
+echo "Loading the pacUpdt timer"
+cd ~/build/
+git clone https://github.com/Michae11s/pacUpdt.git
+cd pacUpdt/
+makepkg -si --noconfirm
+sudo systemctl enable pacUpdt.timer
+
+#PKGS=(
+#'autojump'
+#'awesome-terminal-fonts'
+#'brave-bin' # Brave Browser
+#'dxvk-bin' # DXVK DirectX to Vulcan
+#'github-desktop-bin' # Github Desktop sync
+#'lightly-git'
+#'lightlyshaders-git'
+#'mangohud' # Gaming FPS Counter
+#'mangohud-common'
+#'nerd-fonts-fira-code'
+#'nordic-darker-standard-buttons-theme'
+#'nordic-darker-theme'
+#'nordic-kde-git'
+#'nordic-theme'
+#'noto-fonts-emoji'
+#'papirus-icon-theme'
+#'plasma-pa'
+#'ocs-url' # install packages from websites
+#'sddm-nordic-theme-git'
+#'snapper-gui-git'
+#'ttf-droid'
+#'ttf-hack'
+#'ttf-meslo' # Nerdfont package
+#'ttf-roboto'
+#'zoom' # video conferences
+#'snap-pac'
+#)
+
+echo "Installing AUR Packages"
+for PKG in "${AURPKGS[@]}"; do
+	cd ~/build
+	echo "****************************************************"
+	echo "******************Installing: "$PKG"****************"
+	echo "****************************************************"
+	auracle clone $PKG
+	cd $PKG
+	makepkg -si --noconfirm
 done
-
-export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
-pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
-sleep 1
-konsave -a kde
 
 echo -e "\nDone!\n"
 exit
