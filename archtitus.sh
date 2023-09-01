804550bcc1c707fb3f7a6a1b3d6ee8758e931a9e
Michaels <47094517+Michae11s@users.noreply.github.com>
2023-09-01 15:42:54 -0400

    Update archtitus.sh
    log the install for troubleshooting

--- archtitus.sh
+++ archtitus.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-bash 00-setupvars.sh
+bash 00-setupvars.sh &> install.log
 #read -p "00 Finished. Continue?" con
-bash 0-preinstall.sh
+bash 0-preinstall.sh &>> install.log
 #read -p "0 Finished. Continue?" con
-source ./settings
-arch-chroot /mnt /root/GraniteArch/1-setup.sh
+source ./settings &>> install.log
+arch-chroot /mnt /root/GraniteArch/1-setup.sh  &>> install.log
 #read -p "1 Finished. Continue?" con
-arch-chroot /mnt /usr/bin/runuser -u $USER -- /home/$USER/build/GraniteArch/2-user.sh
+arch-chroot /mnt /usr/bin/runuser -u $USER -- /home/$USER/build/GraniteArch/2-user.sh  &>> install.log
 #read -p "2 Finished. Continue?" con
-arch-chroot /mnt /root/GraniteArch/3-post-setup.sh
+arch-chroot /mnt /root/GraniteArch/3-post-setup.sh  &>> install.log
 rm -r /mnt/root/GraniteArch/
 read -p "Install finished. Reboot? [Y/n]:" Ans
 case $Ans in
